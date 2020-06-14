16ce766762bed40b2db3c5d3f9f3614211c829a2
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-14 21:58:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,21 +1,11 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  new_hash = {}
-  data.each do |key, value|
-    value.each do |new_value, names|
-      names.each do |name|
-        
-        if !new_hash[name]
-          new_hash[name] = {}
-        end
-
-        if !new_hash[name][key]
-          new_hash[name][key] = []
-        end
-
-        new_hash[name][key] << new_value.to_s
-
-      end
-    end
-  end
-  new_hash
+  ##########
+data.each_with_object({}) do |(key, value), final_array|
+ binding.pry
+ value.each do |key
+ final_array
+end      
+  binding.pry
 end
